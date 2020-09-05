6ec42625942c1d49ee2ed55659f00c43a18ca5c9
Diego Abad A <dabada83@gmail.com>
2020-09-05 13:50:12 -0500

    FIX: ir.cron migration to 5.7

---
+++ ir.py
@@ -0,0 +1,13 @@
+from trytond.pool import PoolMeta
+
+
+class Cron(metaclass=PoolMeta):
+    __name__ = 'ir.cron'
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        cls.method.selection.extend([
+                ('galeno.patient.appointment|send_reminder_emails',
+                 "Send remainder emails"),
+                ])
